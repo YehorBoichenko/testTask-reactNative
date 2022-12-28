da2f68d7e7ddfb8bae915107c107164178db710f
Yehor Boichenko <boichenko.yehor@gmail.com>
2022-12-28 12:50:53 +0200

    deploy on expo

---
+++ testApp/README.md
@@ -0,0 +1,1 @@
+App as deployed on expo https://expo.dev/@yehor1ch/testApp?serviceType=classic&distribution=expo-go
--- testApp/babel.config.js
+++ testApp/babel.config.js
@@ -3,15 +3,17 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
     plugins: [
-      "module:react-native-dotenv",
-      {
-        envName: "APP_ENV",
-        moduleName: "@env",
-        path: ".env",
-        safe: false,
-        allowUndefined: true,
-        verbose: false,
-      },
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          safe: false,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
     ],
   };
 };
